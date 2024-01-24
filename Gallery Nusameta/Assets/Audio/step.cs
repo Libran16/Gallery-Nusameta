09f5eb4d39f56d5ec5c78371571760813cdaeaee
DrManggo <153792228+DrManggo@users.noreply.github.com>
2024-01-24 18:42:30 +0700

    anjay

--- Gallery Nusameta/Assets/Audio/step.cs
+++ Gallery Nusameta/Assets/Audio/step.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class step : MonoBehaviour
 {
     public AudioSource footstepsSound, SprintSound;
-
+ 
     void Update()
     {
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)){
